1330fddf077d230100b3dada128660616b44be93
Jhonathan <jhonathan0297@gmail.com>
2025-08-30 11:31:54 -0500

    initialize Spring Boot 3 project

---
+++ src/test/java/com/jhonathan/franchise_rest_api/FranchiseRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhonathan.franchise_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FranchiseRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
