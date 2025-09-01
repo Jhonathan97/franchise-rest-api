3156b2297aeb733ae98b084453c41f64e3b32319
Jhonathan <jhonathan0297@gmail.com>
2025-08-31 21:44:35 -0500

    add terraform and endpoint health

--- iac/terraform/variables.tf
+++ iac/terraform/variables.tf
@@ -1,4 +1,20 @@
-variable "region" { type = string  default = "us-east-1" }
-variable "db_name" { type = string default = "franchises" }
-variable "db_username" { type = string default = "franchise" }
-variable "db_password" { type = string default = "franchise" }
+# variables.tf
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "db_name" {
+  type    = string
+  default = "franchises"
+}
+
+variable "db_username" {
+  type    = string
+  default = "franchise"
+}
+
+variable "db_password" {
+  type    = string
+  default = "franchise"
+}
--- src/main/java/com/jhonathan/franchise_rest_api/web/controllers/HealthController.java
+++ src/main/java/com/jhonathan/franchise_rest_api/web/controllers/HealthController.java
@@ -1,13 +1,25 @@
 package com.jhonathan.franchise_rest_api.web.controllers;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class HealthController {
     @GetMapping("/")
-    public String root() { return "OK"; }
+    public ResponseEntity<?> root() {
+        Map<String,String> body = new HashMap<>();
+        body.put("message","OK");
+        return ResponseEntity.ok(body);
+    }
 
     @GetMapping("/health")
-    public String health() { return "OK"; }
+    public ResponseEntity<?> health() {
+        Map<String,String> body = new HashMap<>();
+        body.put("message","OK");
+        return ResponseEntity.ok(body);
+    }
 }
